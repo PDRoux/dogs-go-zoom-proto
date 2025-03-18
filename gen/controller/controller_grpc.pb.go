126fb3f9adb35837392859ab1087d59b3ef290cf
PD Roux <PaulDermottRoux@gmail.com>
2025-03-18 19:13:31 +0200

    Moved protos to gen folder

--- gen/controller/controller_grpc.pb.go
+++ gen/controller/controller_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v6.30.1
-// source: proto/controller.proto
+// source: controller.proto
 
 package controller
 
@@ -155,5 +155,5 @@ var Controller_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/controller.proto",
+	Metadata: "controller.proto",
 }
